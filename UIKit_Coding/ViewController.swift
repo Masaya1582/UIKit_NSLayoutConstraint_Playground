7ff7d4baa3ebdf3cf46212816aa7de16a4fd1688
Masaya1582 <masa8y88@gmail.com>
2025-10-03 11:35:01 +0900

    Snapkit based Login

--- UIKit_Coding/ViewController.swift
+++ UIKit_Coding/ViewController.swift
@@ -6,87 +6,77 @@
 //
 
 import UIKit
-import SwiftUI
 import SnapKit
+import SwiftUI
 
 final class ViewController: UIViewController {
 
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "ログイン"
-        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 24, weight: .bold)
         label.textAlignment = .center
         return label
     }()
 
     private let idTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "ID"
-        tf.borderStyle = .roundedRect
-        return tf
+        let textField = UITextField()
+        textField.placeholder = "ID"
+        textField.borderStyle = .roundedRect
+        return textField
     }()
 
     private let passwordTextField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "Password"
-        tf.isSecureTextEntry = true
-        tf.borderStyle = .roundedRect
-        return tf
+        let textField = UITextField()
+        textField.placeholder = "パスワード"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .roundedRect
+        return textField
     }()
 
     private let loginButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("ログインする", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        button.setTitle("ログイン", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.backgroundColor = .systemBlue
         return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        configureUIBySnapKit()
+        view.backgroundColor = .lightGray
+        setupConstraints()
     }
 
-    private func configureUIBySnapKit() {
+    private func setupConstraints() {
         [titleLabel, idTextField, passwordTextField, loginButton].forEach {
             view.addSubview($0)
         }
 
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(80)
-            make.centerX.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(40)
         }
 
         idTextField.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(40)
-            make.leading.equalToSuperview().offset(40)
-            make.trailing.equalToSuperview().offset(-40)
+            make.leading.trailing.equalToSuperview().inset(40)
             make.height.equalTo(44)
         }
 
         passwordTextField.snp.makeConstraints { make in
             make.top.equalTo(idTextField.snp.bottom).offset(40)
-            make.leading.equalToSuperview().offset(40)
-            make.trailing.equalToSuperview().offset(-40)
+            make.leading.trailing.equalToSuperview().inset(40)
             make.height.equalTo(44)
         }
 
         loginButton.snp.makeConstraints { make in
             make.top.equalTo(passwordTextField.snp.bottom).offset(40)
-            make.leading.equalToSuperview().offset(40)
-            make.trailing.equalToSuperview().offset(-40)
-            make.height.equalTo(50)
+            make.leading.trailing.equalToSuperview().inset(40)
+            make.height.equalTo(48)
         }
-
-//        let stack = UIStackView(arrangedSubviews: [idTextField, passwordTextField, loginButton])
-//        stack.axis = .vertical
-//        stack.spacing = 20
-//        view.addSubview(stack)
-//
-//        stack.snp.makeConstraints { make in
-//            make.top.equalTo(titleLabel.snp.bottom).offset(40)
-//            make.leading.trailing.equalToSuperview().inset(40)
-//        }
     }
 }
 
